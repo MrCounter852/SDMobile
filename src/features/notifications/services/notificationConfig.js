0d95e2408e5290eaaeb14bf732dcc7d1f132735e
Johan Ardila <johan321q@gamil.com>
2026-01-13 16:05:31 -0500

    cambiando el icono

--- src/features/notifications/services/notificationConfig.js
+++ src/features/notifications/services/notificationConfig.js
@@ -30,7 +30,6 @@ export async function registerForPushNotificationsAsync() {
             importance: Notifications.AndroidImportance.MAX,
             vibrationPattern: [0, 250, 250, 250],
             lightColor: '#FF231F7C',
-            icon: '../../assets/icon.png',
         });
     }
 
